a233134ab8f6896028e63bfd9bf58765f111b269
FilipStraka <straka168@gmail.com>
2024-02-07 22:37:51 +0100

    added check button and trash button to created li element

--- script.js
+++ script.js
@@ -12,8 +12,21 @@ formEl.addEventListener("submit", (event) => {
 
 /* toDoList */
 function toDoList () {
-    let newTask = inputEl.value;
-    const liEl = document.createElement("li");
-    liEl.innerText = newTask;
-    ulEl.appendChild(liEl);
+    let newTask = inputEl.value; //stored input value
+    const liEl = document.createElement("li"); // created li element
+    liEl.innerText = newTask; //stored input value inside created li element
+    ulEl.appendChild(liEl); //appended completed li element
+    inputEl.value =" "; //empty input after submit
+
+    //Check button
+    const checkBtnEl = document.createElement("div"); //created new div element
+    checkBtnEl.innerHTML = `<i class="fas fa-check-square">`; //insert i element in that div
+    liEl.appendChild(checkBtnEl); //append that div > i element 
+
+    //Trash button
+    const trashBtnEl = document.createElement("div");
+    trashBtnEl.innerHTML = `<i class="fas fa-trash">`;
+    liEl.appendChild(trashBtnEl);
+
+    
 }
