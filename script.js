8ddd2c5aea1d2cad3dbd821bd5ebe61ecddd82e3
FilipStraka <straka168@gmail.com>
2024-02-07 22:08:42 +0100

    value of input popup at the end of list after submitting it

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+/* Constants */ 
+
 const formEl = document.querySelector(".form");
-
 const inputEl = document.querySelector(".input");
-
 const ulEl = document.querySelector(".list");
 
+/* Prevented event and called toDoList */ 
 formEl.addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log(inputEl.value);
     toDoList();
-})
+});
 
+/* toDoList */
 function toDoList () {
-    
+    let newTask = inputEl.value;
+    const liEl = document.createElement("li");
+    liEl.innerText = newTask;
+    ulEl.appendChild(liEl);
 }
